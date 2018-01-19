a90e4ea9080cf2a51f9c7aafd0be80fa524da073
Glatoo <darkisharki@gmail.com>
2018-01-19 19:01:11 +0100

    PRe maroska

--- core/src/com/glatoo/game/MainGame.java
+++ core/src/com/glatoo/game/MainGame.java
@@ -4,18 +4,25 @@ import com.badlogic.gdx.ApplicationAdapter;
 
 
 public class MainGame extends ApplicationAdapter {
+	//Premenne si inicializuj este pred funkciami
+	// napr "Texture img;"
 
-	
+	//Takze marosko toto je funkcia ktora sa vola len ked program zacne a to len raz takze tu je najlepsie inicializovat premennym hodnoty
+	//napr "img = new Texture("cat.jpg");"
 	@Override
 	public void create () {
-		
+
 	}
+
+	//Toto je herna slucka, toto je vlastne zakladny cyklus ktory sa opakuje
+	//napr "bash.draw(img,x,y);"
 
 	@Override
 	public void render () {
 
 	}
-	
+	//Toto je dispose tu sa vlastne vzdy mazu nadbytocne veci ako img  a zvuky po ukonceni ich funkcie takze tu je potrevne celkom
+	//robit tento dispose napr "img.dispose();"
 	@Override
 	public void dispose () {
 
